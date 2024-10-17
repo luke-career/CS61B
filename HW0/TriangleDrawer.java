b189a9fa99e6616630befceea642f91f634ea11f
taolu-career <taolu.career@gmail.com>
2024-10-17 14:26:06 -0400

    1

--- HW0/TriangleDrawer.java
+++ HW0/TriangleDrawer.java
@@ -1,10 +1,14 @@
 public class TriangleDrawer {
-    public static void main(String[] args){
-        for(int row = 0; row < 50; row++){
+
+    public static void drawTrianlge(int N){
+        for(int row = 0; row < N; row++){
             for(int colum = 0; colum < row; colum++){
                 System.out.print("*");
             }
             System.out.println("*");
         }
+    }
+    public static void main(String[] args){
+        drawTrianlge(5);
     }
 }
